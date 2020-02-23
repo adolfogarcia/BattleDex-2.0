644875ead465619e33c7dee27f9787140bf8c704
Adolfo <agarcia55512@gmail.com>
2020-02-23 12:59:22 -0800

    changed labels on pkmn labels

--- src/database/parse_repeat_pkmn.js
+++ src/database/parse_repeat_pkmn.js
@@ -9,13 +9,28 @@ for(let key in pokedex){
     )
 
     if(result.length === 0){
-        newList.push(pokedex[key])
+        let oldPkmn = pokedex[key];
+        let newPkmn = {
+            number: oldPkmn.num,
+            name: oldPkmn.species,
+            types: oldPkmn.types,
+            genderRatio: oldPkmn.genderRatio,
+            baseStats: oldPkmn.baseStats,
+            abilities: oldPkmn.abilities,
+            height: oldPkmn.heightm,
+            weight: oldPkmn.weightkg,
+            color: oldPkmn.color,
+            eggGroups: oldPkmn.eggGroups
+        };
+
+        newList.push(newPkmn)
     }
 }
 
 var util = require('util');
 
-
+fs.writeFileSync('./pokedex_filtered.js', 'exports.pokedex = [ \n');
 for(let i = 0; i < newList.length; i++){
     fs.appendFileSync('./pokedex_filtered.js', util.inspect(newList[i]) + ', \n', 'utf-8');
 }
+fs.appendFileSync('./pokedex_filtered.js', ']');
