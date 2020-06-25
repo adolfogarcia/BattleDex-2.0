590cec0952959a223ae6a7145fca0a3c38512d2c
Adolfo <agarcia55512@gmail.com>
2020-06-24 19:02:06 -0700

    made search bar async

--- src/components/HomeSearchBar.js
+++ src/components/HomeSearchBar.js
@@ -1,24 +1,48 @@
-import React from 'react';
-import Select from 'react-select';
+import React, {useState} from 'react';
+import AsyncSelect from 'react-select/async';
+import { useQuery } from '@apollo/react-hooks';
+import { gql } from "apollo-boost";
 
-export default class HomeSearchBar extends React.Component {
-    constructor(){
-        super();
+const GET_ALL_POKEMON = gql`
+    {
+        allPokemon{
+        name
+        }
     }
-      
-    render(){
-        return(
-            <div style={{ bottom: "0px", right:"25vw", padding:"10px", width:"50vw", position: "absolute"}}>
-              <Select 
+`;
+
+export default function HomeSearchBar () {
+    const [maxOptions, setMaxOptions] = useState(100);
+    const { loading, error, data } = useQuery(GET_ALL_POKEMON);
+
+    if (loading) return <p style={{ bottom: "0px", right:"25vw", padding:"10px", width:"50vw", position: "absolute"}}>Loading Pokemon info...</p>;
+    if (error) return <p style={{ bottom: "0px", right:"25vw", padding:"10px", width:"50vw", position: "absolute"}}>Error loading Pokemon</p>;
+
+    const filterOptions = (input) => {
+        return data.allPokemon.filter(pkmn =>
+            pkmn.name.toLowerCase().includes(input.toLowerCase())
+        ).map(pkmn => ({label: pkmn.name, value: pkmn.name}));
+    }
+    
+    const loadOptions = (input, callback) => {
+        setTimeout(() => {
+            callback(filterOptions(input));
+        }, 1000);
+    }    
+
+    return(
+        <div style={{ bottom: "0px", right:"25vw", padding:"10px", width:"50vw", position: "absolute"}}>
+            <AsyncSelect
+                cacheOptions
                 placeholder="Search for Pokemon, moves, abilities, etc..."
+                loadOptions={loadOptions}
                 components={
-                  {
+                    {
                     DropdownIndicator: () => null,
                     IndicatorSeparator: () => null
-                  }
+                    }
                 }
-              />
-            </div>
-        )
-    }
+            />
+        </div>
+    )
 }
